6ba92819c4c84d4880fcf6934f140ab578d5c2fb
Mark Nguyen <mark_nguyen@berkeley.edu>
2024-06-23 18:25:58 -0700

    deployment settings

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-import { v2 as cloudinary} from "cloudinary";
+import { v2 as cloudinary } from "cloudinary";
 
 import authRoutes from "./routes/auth.route.js";
 import userRoutes from "./routes/user.route.js";
@@ -10,20 +11,20 @@ import notificationRoutes from "./routes/notification.route.js";
 
 import connectMongoDB from "./db/connectMongoDB.js";
 
-
 dotenv.config();
 
 cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET,
-})
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 const app = express();
-const PORT = process.env.PORT || 5000; 
+const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
-app.use(express.json({limit:"5mb"})); //middlewear //litmit of poast size
-app.use(express.urlencoded({ extended:true })); //to parse form data
+app.use(express.json({ limit: "5mb" })); //middlewear //litmit of poast size
+app.use(express.urlencoded({ extended: true })); //to parse form data
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
@@ -31,8 +32,15 @@ app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/notifications", notificationRoutes);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
-    console.log(`server is running on port ${PORT}`);
-    connectMongoDB(); 
-})
+  console.log(`server is running on port ${PORT}`);
+  connectMongoDB();
+});
